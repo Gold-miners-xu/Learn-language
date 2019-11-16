d66cde99a3558fe7796cc483305f39e2fdd149ea
Gold-miners-xu <1428333083@qq.com>
2019-11-16 19:09:26 +0800

    乘法口诀表修改

--- 源.cpp
+++ 源.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
-int main()
-{
-	int x,y;
-	for(x=1;x<=9;x++){
+void sheet(int n) {
+	int x, y;
+	for (x = 1; x <= n; x++) {
 		for (y = 1; y <= x; y++) {
-			printf("%2d ", x * y);
-	}
+			printf("%d*%d=%d ", y, x x * y);
+		}
 		printf("\n");
 	}
+}
+int main()
+{
+	int i = 0;
+	scanf("%d", &i);
+	sheet(i);
 	
     return 0;
 }
