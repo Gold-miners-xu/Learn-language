d6f6a989b454f9c5bde970696fbecbdf938c6e68
Gold-miners-xu <1428333083@qq.com>
2020-04-08 22:26:14 +0800

    链表数相加

---
+++ 20年4月/ListAdd/ListAdd/test.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct ListNode {
+	int val;
+	struct ListNode* next;
+	
+};
+typedef struct ListNode Node;
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+	int count = 0;
+	Node* p1 = l1;
+	Node* p2 = l2;
+	Node* head = (Node*)malloc(sizeof(Node));
+	Node* tmp = head;
+	Node* tail = NULL;
+	while (p1 != NULL || p2 != NULL) {
+		if (p1 != NULL && p2 != NULL) {
+			tmp->val = p2->val + p1->val + count;
+
+		}
+		else if (p1 == NULL) {
+			tmp->val = p2->val + count;
+			p2 = p2->next;
+		}
+		else if (p2 == NULL) {
+			tmp->val = p1->val + count;
+			p1 = p1->next;
+		}
+		if (tmp->val >= 10) {
+			tmp->val -= 10;
+			count = 1;
+		}
+		else {
+			count = 0;
+		}
+
+		tmp->next = (Node*)malloc(sizeof(Node));
+		tail = tmp;
+		tmp = tmp->next;
+	}
+	if (count == 1) {
+		tmp->val = 1;
+		tmp->next = NULL;
+	}
+	else {
+		free(tmp);
+		tail->next = NULL;
+	}
+	printf("%d", head->val);
+	return head;
+}
+int main() {
+
+	return 0;
+}
