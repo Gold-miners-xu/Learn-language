abfe5e1d2f05a5b5defedad23a4522ecb6e1286c
Gold-miners-xu <1428333083@qq.com>
2020-04-21 23:32:59 +0800

    二叉树后序遍历

---
+++ 20年4月/postorderTraversal/postorderTraversal/test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+	
+};
+int getSize(struct TreeNode* root) {
+	if (root == NULL) {
+		return 0;
+
+	}
+	return getSize(root->left) + getSize(root->right) + 1;
+}
+void Init(struct TreeNode* root, int* rit, int* size) {
+	if (root) {
+		Init(root->left, rit, size);
+		Init(root->right, rit, size);
+		rit[*size] = root->val;
+		(*size)++;
+
+	}
+}
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+	int* rit = (int*)malloc(sizeof(int) * getSize(root));
+	*returnSize = 0;
+	Init(root, rit, returnSize);
+	return rit;
+}
