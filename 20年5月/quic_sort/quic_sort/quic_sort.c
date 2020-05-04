4b84a83171c91077cdc125add02e07533290f512
Gold-miners-xu <1428333083@qq.com>
2020-05-04 13:31:20 +0800

    quick_sort

--- 20年5月/quic_sort/quic_sort/quic_sort.c
+++ 20年5月/quic_sort/quic_sort/quic_sort.c
@@ -30,3 +30,36 @@ void quic1(int* arr, int i, int j) {
 	quic1(arr, i, left - 1);
 	quic1(arr, left + 1, j);
 }
+void quic2(int* arr, int i, int j) {
+	if (i >= j) {
+		return;
+	}
+	int left = i;
+	int end = j;
+	while (left < end) {
+		while (left < end && arr[i] <= arr[end])
+			end--;
+		while (left < end && arr[i] >= arr[left])
+			left++;
+		swap(arr, left, end);
+
+	}
+	swap(arr, i, left);
+	quic2(arr, i, left - 1);
+	quic2(arr, left + 1, j);
+}
+void quic3(int* arr, int i, int j) {
+	if (i >= j) {
+		return;
+	}
+	int cur = i + 1;
+	int prev = i;
+	while (cur <= j) {
+		if (arr[cur] < arr[i] && ++prev != cur)
+			swap(arr, prev, cur);
+		cur++;
+	}
+	swap(arr, i, prev);
+	quic3(arr, i, prev - 1);
+	quic3(arr, prev + 1, j);
+}
--- 20年5月/quic_sort/quic_sort/test.c
+++ 20年5月/quic_sort/quic_sort/test.c
@@ -1,6 +1,6 @@
 #include"quic_sort.h"
 int main() {
 	int a[] = { 1,15,23,64,8,24,56,3 };
-	quic1(a, 0, 7);
+	quic3(a, 0, 7);
 	return 0;
 }
