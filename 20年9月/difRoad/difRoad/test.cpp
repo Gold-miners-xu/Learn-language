81f411dd2ce73e583da881e0e2557d056b063f00
Gold-miners-xu <1428333083@qq.com>
2020-09-13 22:21:05 +0800

    不同路径

---
+++ 20年9月/difRoad/difRoad/test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int uniquePaths(int m, int n) {
+	if (m == 0 && n == 0)
+		return 0;
+	vector<vector<int>> v;
+	v.resize(n);
+	for (int i = 0; i < n; i++) {
+		v[i].resize(m);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i == 0 || j == 0) {
+				v[i][j] = 1;
+			}
+			else
+				v[i][j] = v[i - 1][j] + v[i][j - 1];
+		}
+	}
+	return v[n - 1][m - 1];
+}
