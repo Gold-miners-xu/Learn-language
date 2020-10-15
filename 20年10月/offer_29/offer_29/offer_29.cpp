d8f1fe3030bbc2fc3ed89f2e52b5fd7ade13f428
Gold-miners-xu <1428333083@qq.com>
2020-10-15 22:40:30 +0800

    顺时针打印矩阵

---
+++ 20年10月/offer_29/offer_29/offer_29.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+	vector<int> rit;
+	if (matrix.size() == 0)
+		return rit;
+	int left = 0, up = 0, right = matrix[0].size() - 1, down = matrix.size() - 1;
+	while (1) {
+		for (int y = left; y <= right; y++) {
+			rit.push_back(matrix[up][y]);
+		}
+		up++;
+		if (down < up)
+			break;
+		for (int x = up; x <= down; x++) {
+			rit.push_back(matrix[x][right]);
+		}
+		right--;
+		if (right < left)
+			break;
+		for (int y = right; y >= left; y--) {
+			rit.push_back(matrix[down][y]);
+		}
+		down--;
+		if (down < up)
+			break;
+		for (int x = down; x >= up; x--) {
+			rit.push_back(matrix[x][left]);
+		}
+		left++;
+		if (right < left)
+			break;
+	}
+	return rit;
+}
